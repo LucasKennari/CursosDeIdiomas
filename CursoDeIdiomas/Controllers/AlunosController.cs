37c71391460c58c408387c1b2dbedbfbe7225526
lucas cezar <lucas.oheisen@gmail.com>
2024-10-26 14:27:09 -0300

    added controllers: aluno and turmas

---
+++ CursoDeIdiomas/Controllers/AlunosController.cs
@@ -0,0 +1,112 @@
+﻿using CursoDeIdiomas.Models;
+using CursoDeIdiomas.Repository;
+using CursoDeIdiomas.Servces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoDeIdiomas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunosController : ControllerBase
+    {
+
+
+        private readonly AlunosService _alunoService;
+        public AlunosController(AlunosService alunoService)
+        {
+           
+            _alunoService = alunoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAlunos()
+        {
+            try
+            {
+                var alunosResponse = await _alunoService.GetAllAsync();
+
+                if (alunosResponse == null)
+                {
+                    return NotFound("Users not found");
+                }
+                return Ok(alunosResponse);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> AlunoRegister([FromBody] AlunosEntity aluno)
+        {
+            try
+            {
+                if (aluno == null)
+                {
+                    return BadRequest("Data aluno is null");
+                }
+
+                var newAluno = await _alunoService.AddNewAluno(aluno);
+                return Created();
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+        /*
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] AlunosEntity? alunoRequest)
+        {
+            try
+            {
+                if (alunoRequest == null)
+                {
+                    return NotFound();
+                }
+                var alunoId = await _alunosRepository.GetByIdAsync(id);
+
+                if (alunoId == null)
+                {
+                    return NotFound();
+                }
+
+                await _alunosRepository.UpdateAsync(alunoId.Id, alunoRequest);
+                return Ok();
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+     
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                if (id == null) return NotFound();
+
+                var alunoId = await _alunosRepository.GetByIdAsync(id);
+
+                if (alunoId == null) return NotFound();
+
+                await _alunosRepository.DeleteAsync(alunoId.Id);
+
+                return Ok();
+
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+      */
+    }
+}
---
+++ CursoDeIdiomas/Controllers/TurmasController.cs
@@ -0,0 +1,79 @@
+﻿using CursoDeIdiomas.DTOs;
+using CursoDeIdiomas.Models;
+using CursoDeIdiomas.Repository;
+using CursoDeIdiomas.Servces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoDeIdiomas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TurmasController : ControllerBase
+    {
+        private readonly TurmasService _turmasService;
+        public TurmasController(TurmasService turmasService)
+        {
+            _turmasService = turmasService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllTurmas()
+        {
+            var turmas = await _turmasService.GetAllAsync();
+            return Ok(turmas);
+        }
+        /*
+                [HttpGet("{id}")]
+                public async Task<IActionResult> GetById(int id)
+                {
+                    var turma = await _turmasService.GetByIdAsync(id);
+                    if (turma == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok(turma);
+                }
+        */
+                [HttpPost]
+                public async Task<IActionResult> Post([FromBody] TurmaRequest turmaRequest)
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        return BadRequest(ModelState);  // Retorna erros de validação
+                    }
+                    TurmasEntity turma = turmaRequest;
+                    var turmaAdded = await _turmasService.AddNewAluno(turmaRequest);
+
+                    if (turmaAdded == null)
+                    {
+                        return BadRequest("The Turma was not created");
+                    }
+                    return Ok(turma);
+                }
+        /*
+                [HttpPut("{id}")]
+                public async Task<IActionResult> Put(int id, [FromBody] TurmaRequest turmaDTO)
+                {
+                    var result = await _turmasRepository.UpdateAsync(id, turmaDTO);
+
+                    if (result == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok();
+                }
+
+                [HttpDelete("{id}")]
+                public async Task<IActionResult> Delete(int id)
+                {
+
+                    var result = await _turmasRepository.DeleteAsync(id);
+                    if (result == null)
+                    {
+                        return NotFound();
+                    }
+                    return Ok();
+                }
+        */
+    }
+
+}
