92ee368841366bd2c1c6b8fecfdfee7d56053a9e
lucas cezar <lucas.oheisen@gmail.com>
2024-10-26 14:30:05 -0300

    added services; turmas and alunos

---
+++ CursoDeIdiomas/Servces/AlunosService.cs
@@ -0,0 +1,44 @@
+﻿using CursoDeIdiomas.DTOs;
+using CursoDeIdiomas.Models;
+using CursoDeIdiomas.Repository;
+
+namespace CursoDeIdiomas.Servces
+{
+    public class AlunosService
+    {
+      private readonly AlunosRepository _alunosRepository;
+        public AlunosService(AlunosRepository alunosRepository)
+        {
+            _alunosRepository = alunosRepository;
+        }
+        public async Task<List<AlunoResponse>> GetAllAsync()
+        {
+        var aluno = await _alunosRepository.GetAllAsync();
+
+            if (aluno == null)
+            {
+                return null;
+            }
+
+            var alunoResponse = aluno.Select(aluno => new AlunoResponse
+            {
+                Id = aluno.Id,
+                Nome = aluno.Nome,
+                Cpf = aluno.Cpf,
+                Email = aluno.Email,
+                Turmas = aluno.AlunoTurmas.Select(turma => new TurmaResponse
+                {
+                    Codigo = turma.Turma.Codigo,
+                    Nivel = turma.Turma.Nivel 
+                }).ToList()
+            }).ToList();
+
+            return alunoResponse;
+        }
+        public async Task<AlunosEntity> AddNewAluno(AlunosEntity aluno)
+        {
+            await _alunosRepository.AddAsync(aluno);
+            return aluno;
+        }
+    }
+}
---
+++ CursoDeIdiomas/Servces/TurmasService.cs
@@ -0,0 +1,47 @@
+﻿using CursoDeIdiomas.DTOs;
+using CursoDeIdiomas.Models;
+using CursoDeIdiomas.Repository;
+
+namespace CursoDeIdiomas.Servces
+{
+    public class TurmasService
+    {
+        private readonly TurmasRepository _turmasRepository;
+        public TurmasService(TurmasRepository turmasRepository)
+        {
+            _turmasRepository = turmasRepository;
+        }
+        public async Task<List<TurmaResponse>> GetAllAsync()
+        {
+            var turmas = await _turmasRepository.GetAllAsync();
+
+            if (turmas == null)
+            {
+                return null;
+            }
+            return turmas.Select(turma => (TurmaResponse)turma).ToList();
+            /*  var turmaResponse = turmas.Select(turma => new TurmaResponse
+              {
+                  Codigo = turma.Codigo,
+                  Nivel = turma.Nivel,
+                  Alunos = turma.Alunos.Select(aluno => new AlunoResponse
+                  {
+                      Id = aluno.,
+                      Nome = aluno,
+                      Cpf = aluno.Cpf,
+                      Email = aluno.Email
+                  }).ToList()
+              }).ToList();
+
+
+              return turmaResponse;
+            */
+        }
+        public async Task<TurmasEntity> AddNewAluno(TurmaRequest turmaRequest)
+        {
+      
+
+            return await _turmasRepository.AddAsync(turmaRequest);
+        }
+    }
+}
